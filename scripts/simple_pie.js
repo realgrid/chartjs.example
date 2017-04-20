6cc848202c73f59f89001ddfc311e139fa786f27
jun <jun@gmail.com>
2017-04-20 10:37:42 +0900

    package initial

---
+++ scripts/simple_pie.js
@@ -0,0 +1,27 @@
+function createChart(element) {
+    var ctx = document.getElementById(element);
+    var chartView = new Chart(ctx, {
+        type: 'pie',
+        data: {
+            labels: [
+                "Red",
+                "Blue",
+                "Yellow"
+            ],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ]
+                }]
+        }
+    });
+}
